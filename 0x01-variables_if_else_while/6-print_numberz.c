39eb3fc2b95b3c90b2e283cfccab6d18e6f97123
ugwuNoah <ugwujahswill@gmail.com>
2022-09-09 23:38:26 +0000

    This is the second project in c language

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9
+ *
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
