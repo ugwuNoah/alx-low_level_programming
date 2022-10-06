15d80030641585a7993f597438ac076460b5356c
ugwuNoah <ugwujahswill@gmail.com>
2022-10-06 21:31:47 +0000

    more on malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: nothing
+ */
+int main(int argc, char **argv)
+{
+	int  num1, num2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+		return (0);
+	}
+}
